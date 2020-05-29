831f41fc12e2fe9dd6e577c15be7b948f76cfe79
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-05-30 00:03:55 +0100

    Building a Layout file

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,5 +1,8 @@
+const layout = require('../layout');
+
 module.exports = () => {
-    return `
+    return layout({
+        content :`
         <div>
             <form method="POST">
                 <input name="email" placeholder="email" />
@@ -7,5 +10,5 @@ module.exports = () => {
                 <button>Sign In</button>
             </form>
         </div>
-    `;
+    `});
 };
