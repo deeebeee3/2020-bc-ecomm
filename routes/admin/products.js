b0178e934c76a72c89c04b09f21bb1fee7e3ef1f
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-06-01 23:12:22 +0100

    saving the image

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 const multer = require('multer');
 
-const ProductsRepository = require('../../repositories/products');
+const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
@@ -17,11 +17,15 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'),  (req, res) => {
+router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'),  async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
     console.log(req.body);
     console.log(req.file);
+
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
 
     res.send('submitted');
 });
