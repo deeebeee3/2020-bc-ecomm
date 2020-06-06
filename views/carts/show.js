7c262ea43ce486fe98dc226104f536ff532afcec
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-06-06 12:48:07 +0100

    Rendering the list

--- views/carts/show.js
+++ views/carts/show.js
@@ -3,7 +3,7 @@ const layout = require('../layout');
 module.exports = ({ items }) => {
     const renderedItems = items.map((item) => {
         return `
-            <div>${item.product.title} - ${item.product.price}</div>
+            <div>${item.product.title} - ${item.product.price} - ${item.quantity}</div>
         `;
     }).join('');
 
@@ -12,5 +12,5 @@ module.exports = ({ items }) => {
             <h1>Cart</h1>
             ${renderedItems}
         `
-    })
+    });
 };
